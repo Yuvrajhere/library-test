179f640a8814084936418db0e8a77d4f69f219fd
Rachana Maheshwari <yuvrajisbest13@gmail.com>
2022-08-11 18:28:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello word!")
+function Test() {
+    // let _this = this;
+    this.printHello = () => {
+        const body = document.querySelector("body");
+        const div = document.createElement("div");
+        div.textContent = "Hello world";
+
+        body.appendChild(div);
+    };
+}
+
+const test = new Test();
+
+module.exports = {
+    printHello: test.printHello,
+};
